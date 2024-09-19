c0f099cdc8ed46d54c02bfec80be4c7afaa4cea4
186483 <alexander.h.log@gmail.com>
2024-09-19 22:59:03 +0200

    Leverer inn

--- src/no/hvl/dat100/O2.java
+++ src/no/hvl/dat100/O2.java
@@ -12,24 +12,24 @@ public class O2 {
 		double trinnskatt = 0;
 
 		if (bruttoinntekt > 1350000) {
-            trinnskatt += (bruttoinntekt - 1350000) * 0.176;
-            bruttoinntekt = 1350000;
-        }
-        if (bruttoinntekt > 937900) {
-            trinnskatt += (bruttoinntekt - 937900) * 0.166;
-            bruttoinntekt = 937900;
-        }
-        if (bruttoinntekt > 670000) {
-            trinnskatt += (bruttoinntekt - 670000) * 0.136;
-            bruttoinntekt = 670000;
-        }
-        if (bruttoinntekt > 292850) {
-            trinnskatt += (bruttoinntekt - 292850) * 0.04;
-            bruttoinntekt = 292850;
-        }
-        if (bruttoinntekt > 208050) {
-            trinnskatt += (bruttoinntekt - 208050) * 0.017;
-        }
+			trinnskatt += (bruttoinntekt - 1350000) * 0.176;
+			bruttoinntekt = 1350000;
+		}
+		if (bruttoinntekt > 937900) {
+			trinnskatt += (bruttoinntekt - 937900) * 0.166;
+			bruttoinntekt = 937900;
+		}
+		if (bruttoinntekt > 670000) {
+			trinnskatt += (bruttoinntekt - 670000) * 0.136;
+			bruttoinntekt = 670000;
+		}
+		if (bruttoinntekt > 292850) {
+			trinnskatt += (bruttoinntekt - 292850) * 0.04;
+			bruttoinntekt = 292850;
+		}
+		if (bruttoinntekt > 208050) {
+			trinnskatt += (bruttoinntekt - 208050) * 0.017;
+		}
 
 		JOptionPane.showMessageDialog(null, "Trinnskatten er: " + trinnskatt + " kr");
 	}
